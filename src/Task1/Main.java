bacc6f6425540ed3b7a8d53d01d7b580a88351e1
Admin <Admin@DESKTOP-FQ0FFHA>
2023-03-22 20:06:55 +0300

    task

---
+++ src/Task1/Main.java
@@ -0,0 +1,10 @@
+package Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Я");
+        System.out.println("Хорошо");
+        System.out.println("Знаю");
+        System.out.println("Java");
+    }
+}
